3504f1d9a4ddfad6dc848782bb9ff417b8953fb8
Maximilian Hammerl <maximilian@hammerl.dev>
2023-03-27 20:31:00 +0200

    Added CredentialManager dependency and utility to remember username and password

---
+++ PostExporter/Utils/CredentialUtil.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Controls;
+using Meziantou.Framework.Win32;
+
+namespace PostExporter.Utils;
+
+public static class CredentialUtil
+{
+    public static void UpdateCredential(string username, string password)
+    {
+        CredentialManager.WriteCredential(
+            applicationName: Util.GetAppName(),
+            userName: username,
+            secret: password,
+            persistence: CredentialPersistence.LocalMachine);
+    }
+
+    public static (string Username, string Password)? ReadCredential()
+    {
+        var credential = CredentialManager.ReadCredential(applicationName: Util.GetAppName());
+        if (credential is null || credential.UserName is null || credential.Password is null)
+        {
+            return null;
+        }
+        return (Username: credential.UserName, credential.Password);
+    }
+
+    public static void DeleteCredential()
+    {
+        CredentialManager.DeleteCredential(applicationName: Util.GetAppName());
+    }
+}
